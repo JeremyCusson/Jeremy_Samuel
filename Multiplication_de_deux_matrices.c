71c3354bc06caefbaa29022992b18336bacc7c37
JeremyCusson <65480663+JeremyCusson@users.noreply.github.com>
2020-10-04 17:38:56 -0400

    Update Multiplication_de_deux_matrices.c

--- Multiplication_de_deux_matrices.c
+++ Multiplication_de_deux_matrices.c
@@ -3,15 +3,15 @@
  Auteur: Jérémy Cusson cusj3102
  Date: 02 octobre 2020
  Description: Calcul d'une multiplication de deux matrices carrées
-              et tests du fonctionnement du codage
+              et tests du limitage du codage
 ********/
 
 #include <stdio.h>
 #define n 5
 
-void TestMultiplicationMatrices(double matrice1[n][n], double matrice2[n][n], double matrice3[n][n]);
+void MultiplicationMatrices(double matrice1[n][n], double matrice2[n][n], double matrice3[n][n]);
 
-// Description: Opération de la multiplication de deux matrices
+// Description: Test pour l'opération de la multiplication de deux matrices
 int main(void)
 {	
 	//Test pour vérifier l'aboutissement du code avec des valeurs extrèmes dans la matrice
@@ -29,7 +29,7 @@ int main(void)
 		}
 	}
 	
-	TestMultiplicationMatrices(matrice1, matrice2, matrice3);
+	MultiplicationMatrices(matrice1, matrice2, matrice3);
 	
 	printf("-------------------------------------------\n");
 	
@@ -51,13 +51,13 @@ int main(void)
 		}
 	}
 	
-	TestMultiplicationMatrices(matrice1, matrice2, matrice3);
+	MultiplicationMatrices(matrice1, matrice2, matrice3);
 	printf("-------------------------------------------\n");
 	
 	//Test pour vérifier que le programme affiche des chiffres en mettant des lettres
 	double matriceTest1[n][n] = {{'q','z','a','n','m'},{'h','l','a','r','d'},{'v','x','a','v','x'},{'g','f','t','a','o'},{'k','j','a','h','j'}};
 	double matriceTest2[n][n] = {{'q','z','a','n','m'},{'h','l','a','r','d'},{'v','x','a','v','x'},{'g','f','t','a','o'},{'k','j','a','h','j'}};
-	TestMultiplicationMatrices(matriceTest1, matriceTest2, matrice3);
+	MultiplicationMatrices(matriceTest1, matriceTest2, matrice3);
 	printf("-------------------------------------------\n");
 	
 	//Test pour vérifier que le programme affiche des valeurs avec des chiffres négatifs élevés
@@ -66,18 +66,19 @@ int main(void)
 	double matriceTest4[n][n] = {{-1343,-2324,-332432,-4434,-5324234},{-632432,-74324,-832423,-9432,-1043242},{-114324,-126565,-13435,-14432,-1532},
 		                         {-1654,-1732,-183432,-19432,-2033},{-21423,-22432,-23432,-2432,-25432}};
 	
-	TestMultiplicationMatrices(matriceTest3, matriceTest4, matrice3);
+	MultiplicationMatrices(matriceTest3, matriceTest4, matrice3);
 	printf("-------------------------------------------\n");
 	
 	return 0;
 }
 
-//Description: Tests, selon les matrices paramétrées
+//Description: Multiplication de deux matrices paramétrées
 //Précondition: Matrice carré avec le même nombre de colonnes et de lignes
-//Postecondition: Matrice résultante possède le même nombre de colonnes et de lignes que les autres matrices
-void TestMultiplicationMatrices(double matrice1[n][n], double matrice2[n][n], double matrice3[n][n])
+//Postecondition: Matrice résultante possède le même nombre de colonnes et de lignes
+void MultiplicationMatrices(double matrice1[n][n], double matrice2[n][n], double matrice3[n][n])
 {
-	int nombreColonneLigne = 0;
+	int nombreColonne = 0;
+	int nombreLigne = 0;
 	
 	//Fait la multiplication de deux matrices
 	for(int i = 0; i < n; i++)
@@ -89,12 +90,13 @@ void TestMultiplicationMatrices(double matrice1[n][n], double matrice2[n][n], do
 				matrice3[i][j] = matrice3[i][j] + (matrice1[i][k]*matrice2[k][j]);
 			}
 			printf("%f  ", matrice3[i][j]);
+			nombreColonne++;
 		}
 		printf("\n");
-		nombreColonneLigne++;
+		nombreLigne++;
 	}
 	
-	if (nombreColonneLigne != n)
+	if (nombreColonne != nombreLigne)
 	{
 		printf("Erreur");
 		matrice3 = 0;
